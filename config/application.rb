9b1220e8a0b7c98a46f6023eda2bb1ba84f987bb
Rafael Aquino <rafael.acerqueirah@gmail.com>
2024-11-22 19:50:58 -0300

    Config locales

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module FitnessDiet
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    I18n.available_locales = [:en]
+
+    config.i18n.default_locale = :en
   end
 end
